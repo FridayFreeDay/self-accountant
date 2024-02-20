f47c25a960d7affc6aa92dbbf641b44111425b03
Ilya <greengableskind@gmail.com>
2024-02-20 23:27:20 +0300

    Undeploy

--- accountant/staticfiles/js/script.js
+++
@@ -1,4 +0,0 @@
-// Скрипт для прокрутки страницы к якорю #table_id при загрузке страницы
-// $(document).ready(function(){
-//     $('html, body').animate({ scrollTop: $("#table_id").offset().top }, 300);
-//      });
