2580d424004bd10110fbf107d3c3bd4b5a2ee760
Ilya <greengableskind@gmail.com>
2024-02-20 23:20:57 +0300

    Deploy

--- accountant/accountant/settings.py
+++ accountant/accountant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o1kvgfzb88oad&&fl!i3eh=ttxh4i)+%q(s5rjl(547s-3tguf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = ["127.0.0.1",]
 
 # Application definition
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
---
+++ accountant/staticfiles/js/script.js
@@ -0,0 +1,4 @@
+// Скрипт для прокрутки страницы к якорю #table_id при загрузке страницы
+// $(document).ready(function(){
+//     $('html, body').animate({ scrollTop: $("#table_id").offset().top }, 300);
+//      });
