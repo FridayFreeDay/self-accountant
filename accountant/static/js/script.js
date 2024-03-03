421b52e0b0bdde81026781da6531ea4c030894f9
Ilya <greengableskind@gmail.com>
2024-03-03 15:00:30 +0300

    Добавлена анимация при переходе между страницами

--- accountant/static/js/script.js
+++ accountant/static/js/script.js
@@ -2,3 +2,17 @@
 // $(document).ready(function(){
 //     $('html, body').animate({ scrollTop: $("#table_id").offset().top }, 300);
 //      });
+
+$(window).on('load pageshow', function () {
+    $('body').fadeIn();
+});    
+$("a:not([href*=\\#])").click(function() {
+    if (!$(this).attr('target')) {
+        $('body').fadeOut();
+        let url = $(this).attr('href');
+        window.setTimeout(function() {
+            window.location.href = url;
+        }, 500);    
+        return false;
+    }
+});
